f4247cdbfe0d8529fc7ed16bcfd3907c7a91d7d7
gusenok-y <llomm@tut.by>
2022-06-16 17:11:34 +0300

    Homework 9

--- src/HomeWork.java
+++ src/HomeWork.java
@@ -4,22 +4,20 @@ import java.util.stream.Collectors;
 public class HomeWork {
 
     public static void main(String[] args) {
-        List<Student> students = Data.getStudentsList(); // в данном случае класс Data содержит тестовые данные, у вас они будут свои
-        Course randomCourse = Data.getRandomCourse(); // ваша реализация подачи случайного курса, можно просто создать объект и его передавать
+        List<Student> students = Data.getStudentsList();
+        Course randomCourse = Data.getRandomCourse();
         System.out.println(getUnicsCourses(students));
         System.out.println(inquisitiveStudents(students));
         System.out.println(getStudentsFromCourse(students, randomCourse));
     }
 
-    // 1. Написать функцию, принимающую список Student и возвращающую список уникальных курсов, на которые подписаны студенты.
-    public static List<Course> getUnicsCourses(List<Student> students) {
+   public static List<Course> getUnicsCourses(List<Student> students) {
         return students.stream()
                 .flatMap(student -> student.getAllCourses().stream())
                 .distinct()
                 .collect(Collectors.toList());
     }
 
-    // 2. Написать функцию, принимающую на вход список Student и возвращающую список из трех самых любознательных (любознательность определяется количеством курсов).
     public static List<Student> inquisitiveStudents (List<Student> students) {
         return students.stream()
                 .sorted((s1, s2) -> (s2.getAllCourses().size()) - (s1.getAllCourses().size()))
@@ -27,7 +25,6 @@ public class HomeWork {
                 .collect(Collectors.toList());
     }
 
-    // 3. Написать функцию, принимающую на вход список Student и экземпляр Course, возвращающую список студентов, которые посещают этот курс.
     public static List<Student> getStudentsFromCourse(List<Student> students, Course course) {
         System.out.println("Ищем студентов проходящих курс " + course);
         return students.stream()
